52e80c2d8ec4f6253f509f417cb267fdbc60961c
vincenzo <vincenzo.longo@acsoftware.it>
2020-11-16 16:41:05 +0100

    Add hyperiot-core-clustered feature in file configuration

--- modules/HyperIoTHadoopManager/HyperIoTHadoopManager-test/src/test/java/it/acsoftware/hyperiot/hadoopmanager/test/HyperIoTHadoopManagerConfiguration.java
+++ modules/HyperIoTHadoopManager/HyperIoTHadoopManager-test/src/test/java/it/acsoftware/hyperiot/hadoopmanager/test/HyperIoTHadoopManagerConfiguration.java
@@ -19,17 +19,67 @@ public class HyperIoTHadoopManagerConfiguration {
                 // HADOOPMANAGER PROPERTIES
                 editConfigurationFilePut("etc/it.acsoftware.hyperiot.hadoopmanager.cfg",
                         "it.acsoftware.hyperiot.hadoopmanager.defaultFS", "${env:HYPERIOT_HADOOPMANAGER_DEFAULTFS:-hdfs://namenode:8020}"),
+                // ALGORITHM PROPERTIES
+                editConfigurationFilePut("etc/it.acsoftware.hyperiot.algorithm.cfg",
+                        "it.acsoftware.hyperiot.algorithm.jar.base.path", "${env:HYPERIOT_ALGORITHM_JAR_BASE_PATH:-/spark/jobs}"),
                 new KarafDistributionConfigurationFileExtendOption(
                         new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
-                        ",mvn:it.acsoftware.hyperiot.hadoopmanager/HyperIoTHadoopManager-features/" + hyperiotVersion
+                        ",mvn:it.acsoftware.hyperiot.dashboard/HyperIoTDashboard-features/" + hyperiotVersion
+                                + "/xml/features"),
+                new KarafDistributionConfigurationFileExtendOption(
+                        new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
+                        ",mvn:it.acsoftware.hyperiot.dashboard.widget/HyperIoTDashboardWidget-features/" + hyperiotVersion
+                                + "/xml/features"),
+                new KarafDistributionConfigurationFileExtendOption(
+                        new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
+                        ",mvn:it.acsoftware.hyperiot.hproject/HyperIoTHProject-features/" + hyperiotVersion
+                                + "/xml/features"),
+                new KarafDistributionConfigurationFileExtendOption(
+                        new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
+                        ",mvn:it.acsoftware.hyperiot.hdevice/HyperIoTHDevice-features/" + hyperiotVersion
+                                + "/xml/features"),
+                new KarafDistributionConfigurationFileExtendOption(
+                        new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
+                        ",mvn:it.acsoftware.hyperiot.hpacket/HyperIoTHPacket-features/" + hyperiotVersion
+                                + "/xml/features"),
+                new KarafDistributionConfigurationFileExtendOption(
+                        new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
+                        ",mvn:it.acsoftware.hyperiot.stormmanager/HyperIoTStormManager-features/" + hyperiotVersion
+                                + "/xml/features"),
+                new KarafDistributionConfigurationFileExtendOption(
+                        new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
+                        ",mvn:it.acsoftware.hyperiot.rule/HyperIoTRuleEngine-features/" + hyperiotVersion
+                                + "/xml/features"),
+                new KarafDistributionConfigurationFileExtendOption(
+                        new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
+                        ",mvn:it.acsoftware.hyperiot.area/HyperIoTArea-features/" + hyperiotVersion
+                                + "/xml/features"),
+                new KarafDistributionConfigurationFileExtendOption(
+                        new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
+                        ",mvn:it.acsoftware.hyperiot.hbase.connector/HyperIoTHBaseConnector-features/" + hyperiotVersion
                                 + "/xml/features"),
                 new KarafDistributionConfigurationFileExtendOption(
                         new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
                         ",mvn:it.acsoftware.hyperiot.core/HyperIoTCore-features/" + hyperiotVersion
                                 + "/xml/features"),
                 new KarafDistributionConfigurationFileExtendOption(
+                        new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
+                        ",mvn:it.acsoftware.hyperiot.hproject.algorithm/HyperIoTHProjectAlgorithm-features/" + hyperiotVersion
+                                + "/xml/features"),
+                new KarafDistributionConfigurationFileExtendOption(
+                        new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
+                        ",mvn:it.acsoftware.hyperiot.algorithm/HyperIoTAlgorithm-features/" + hyperiotVersion
+                                + "/xml/features"),
+                new KarafDistributionConfigurationFileExtendOption(
+                        new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresRepositories"),
+                        ",mvn:it.acsoftware.hyperiot.hadoopmanager/HyperIoTHadoopManager-features/" + hyperiotVersion
+                                + "/xml/features"),
+                new KarafDistributionConfigurationFileExtendOption(
                         new ConfigurationPointer("etc/org.apache.karaf.features.cfg", "featuresBoot"),
-                        "hyperiot-hadoopmanager,hyperiot-core,"
+                        ",hyperiot-algorithm,hyperiot-hadoopmanager,hyperiot-hprojectalgorithm,hyperiot-core-clustered," +
+                                "hyperiot-dashboard,hyperiot-dashboardwidget,hyperiot-hproject,hyperiot-hdevice," +
+                                "hyperiot-hpacket,hyperiot-ruleengine,hyperiot-area,hyperiot-stormmanager," +
+                                "hyperiot-hbaseconnector"
                 )
         };
     }
