25a2119f8515217bdd5954b040124f8c4f37b70a
giacomo <giacomo.spanp@acsoftware.it>
2020-11-23 09:45:42 +0100

    Add methods to HyperIoTJob interface

--- modules/HyperIoTBase/HyperIoTBase-api/src/main/java/it/acsoftware/hyperiot/base/api/entity/HyperIoTJob.java
+++ modules/HyperIoTBase/HyperIoTBase-api/src/main/java/it/acsoftware/hyperiot/base/api/entity/HyperIoTJob.java
@@ -1,5 +1,38 @@
 package it.acsoftware.hyperiot.base.api.entity;
 
+import org.quartz.JobDetail;
+import org.quartz.JobKey;
+
+import java.util.Map;
+
+/**
+ * This interface put the attention on mandatory information required by Quartz scheduler
+ */
 public interface HyperIoTJob {
+
+    /**
+     * It returns implementation classname of org.quartz.Job interface
+     * @return implementation classname of org.quartz.Job interface
+     */
+    String getClassName();
+
+    /**
+     * It returns job cron expression
+     * @return job cron expression
+     */
+    String getCronExpression();
+
+    /**
+     * It returns detail of job, i.e. an instance of org.quartz.JobDetail class
+     * @return Detail of job
+     */
+    JobDetail getJobDetail();
+
+    /**
+     * It returns identification of Job, i.e. an instance of org.quartz.JobKey class
+     * @return Key of job
+     */
+    JobKey getJobKey();
+    Map<String, Object> getJobParams();
 
 }
