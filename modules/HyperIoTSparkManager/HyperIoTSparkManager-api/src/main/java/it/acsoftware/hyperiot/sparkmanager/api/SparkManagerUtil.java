8c4b874fee9cc11e8ca3ffb30898f771d50213e2
giacomo <giacomo.spanp@acsoftware.it>
2020-12-07 14:16:43 +0100

    Add methods to retrieve properties from cfg

--- modules/HyperIoTSparkManager/HyperIoTSparkManager-api/src/main/java/it/acsoftware/hyperiot/sparkmanager/api/SparkManagerUtil.java
+++ modules/HyperIoTSparkManager/HyperIoTSparkManager-api/src/main/java/it/acsoftware/hyperiot/sparkmanager/api/SparkManagerUtil.java
@@ -2,7 +2,12 @@ package it.acsoftware.hyperiot.sparkmanager.api;
 
 public interface SparkManagerUtil {
 
+    String getSparkClientVersion();
+    boolean getSparkDriverSupervise();
+    int getSparkEnvLoaded();
     String getSparkMasterHostname();
     int getSparkRestApiPort();
+    String getSparkRestApiUrl();
+    String getSparkSubmitDeployMode();
 
 }
